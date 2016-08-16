be6976d9200316ad3b8efaf4a0055dce107e6e81
Emeric Henon <emeric.henon@gmail.com>
2016-08-16 14:16:14 +0200

    Update projections_controller.rb
    J ai virer tes strong params

--- app/controllers/projections_controller.rb
+++ app/controllers/projections_controller.rb
@@ -3,11 +3,4 @@ class ProjectionsController < ApplicationController
   def show
     @projection = Projection.find(params[:id])
   end
-
-  private
-
-  def projection_params
-    params.require(:projection).permit(:movie, :address, :date, :capacity )
-  end
-
 end
