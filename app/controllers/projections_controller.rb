13698b79cc9d105abe90355330de23de962569ba
lucmonteil <luc-monteil.esc@em-lyon.com>
2016-08-16 13:07:12 +0200

    route projection#show added, created in controller, view created

--- app/controllers/projections_controller.rb
+++ app/controllers/projections_controller.rb
@@ -1,2 +1,13 @@
 class ProjectionsController < ApplicationController
+
+  def show
+    @projection = Projection.find(params[:id])
+  end
+
+  private
+
+  def projection_params
+    params.require(:projection).permit(:movie, :address, :date, :capacity )
+  end
+
 end
