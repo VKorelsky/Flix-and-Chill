aa72e16301b707edd337483df2eb7277c79147c8
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-08-16 15:55:15 +0200

    basic show logic implemented

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   resources :projections, only: [:new, :create, :show]
 
+  resources :users, only: [:show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
