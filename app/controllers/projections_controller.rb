5cab7632a5925ba637eb7b61105d2adbbba3fb45
Emeric Henon <emeric.henon@gmail.com>
2016-08-16 13:56:15 +0200

    new and create done but cannot test because of lack of #show

--- app/controllers/projections_controller.rb
+++ app/controllers/projections_controller.rb
@@ -1,2 +1,24 @@
 class ProjectionsController < ApplicationController
+  # before_action :set_projections, only: [:new, :create]
+
+  def new
+    @projection = Projection.new
+  end
+
+  def create
+    @projection = Projection.new(projection_params)
+    # @projection[:user] = @user => il faut remplir le user_id
+    @projection.save
+    redirect_to projection_path(@projection)
+  end
+
+  private
+
+  # def set_projections
+  #    @projection = Projection.find(params[:id])
+  # end
+
+  def projection_params
+    params.require(:projection).permit(:name, :address, :movie, :date, :capacity)
+  end
 end
