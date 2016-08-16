8beee7a667494010e2c0858a57a5ccdd3329e6d8
gildas fremont <gildas.fremont@gmail.com>
2016-08-16 14:43:18 +0200

    Seed, Destroy dependents, projections index action, projections index view

--- app/controllers/projections_controller.rb
+++ app/controllers/projections_controller.rb
@@ -1,2 +1,7 @@
 class ProjectionsController < ApplicationController
+  def index
+    today = Date.today
+    @futur_projections = Projection.where("date > ?", today).order(date: :desc)
+    @past_projections = Projection.where("date > ?", today).order(date: :desc)
+  end
 end
