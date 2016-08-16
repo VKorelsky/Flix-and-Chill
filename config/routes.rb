bd3e4d48a4953f4e7afb5d7b08c09c4e07461611
Emeric Henon <emeric.henon@gmail.com>
2016-08-16 16:43:55 +0200

    add routes, update and edit are working

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :projections, only: [:new, :create, :show, :index]
+  resources :projections, only: [:new, :create, :show, :index, :edit, :update]
 end
