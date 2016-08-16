a9fdbcb5e9c713d1bf8941c12d52cf310ee0f4b1
Emeric Henon <emeric.henon@gmail.com>
2016-08-16 11:48:35 +0200

    update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,13 @@
 class Review < ApplicationRecord
   belongs_to :booking
-  belongs_to :user
-  belongs_to :projection
+  # TODO: belongs_to :user
 
   #validations
 
   validates :rating, inclusion: {in: 0..5}
   validates :content, length: {minimum: 40, message: "Your review must be at least 40 characters long"}
+
+  # def projection
+  #   booking.projection
+  # end
 end
