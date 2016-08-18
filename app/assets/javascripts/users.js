b911bd1c84564791c5676ca0fd0ba4d96fdf2e9a
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2016-08-18 18:18:57 +0200

    js logic done

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,10 +5,14 @@ $(document).ready( function(){
   $('#activity-history').on("click", function() {
     $(this).toggleClass("active");
     $('#future-activity').toggleClass("active");
+    $('#future-projections').addClass("hidden");
+    $('#past-projections').removeClass("hidden");
   });
 
   $('#future-activity').on("click", function() {
     $(this).toggleClass("active");
     $('#activity-history').toggleClass("active");
+    $('#past-projections').addClass("hidden");
+    $('#future-projections').removeClass("hidden");
   });
 });
